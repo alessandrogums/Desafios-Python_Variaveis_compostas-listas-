1a3e8116c05340c59045e08098ce8df20213d5cc
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-09-14 10:54:14 -0300

    Update Exercicio.79_CV.py

--- Exercicio.79_CV.py
+++ Exercicio.79_CV.py
@@ -1,23 +1,16 @@
 # Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. 
 # Caso o número já exista lá dentro, ele não será adicionado. 
-# No final, serão exibidos todos os valores únicos digitados, em ordem crescente(Não pode usar o comando .sort)
+# No final, serão exibidos todos os valores únicos digitados, em ordem crescente
 
 lista_numeros=[]
-for c in range(0,5):
-    num=int(input(f'digite o valor do nº{c}:'))
-    if c==0:
-        lista_numeros.append(num)
-        print('o número foi colocado na última posição!')
-    else:
-        if num>=lista_numeros[c-1]:
-            lista_numeros.insert(c,num)
-            print(f'o número foi colocado na posição {c}')
-        else:
-            cont=c
-            while num<lista_numeros[cont-1]:
-                cont-=1
-                if cont==0:
-                    break
-            lista_numeros.insert(cont,num)
-            print(f'o número foi colocado na posição {cont}')
-print(lista_numeros)
+while True:
+    num=float(input('digite um número:'))
+    while  num in lista_numeros:
+        print('este número já foi adicionado na lista!')
+        num=float(input('digite outro número novamente:'))
+    lista_numeros.append(num)
+    escolha=str(input('quer continuar?digite sim ou não:')).strip().upper()[0]
+    if escolha =='N':
+        break
+lista_numeros.sort()
+print(f'os valores adicionados em ordem crescente foram:{lista_numeros}')
